124e1eb81453e9599b0097a38a6ef33d7d020e5a
yaswantS542297 <77638984+yaswantS542297@users.noreply.github.com>
2021-03-24 19:43:29 -0500

    updated users.js file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var value = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value++;
+  res.send(`User accesses are: ${value}`);
 });
 
 module.exports = router;
