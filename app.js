1de0946919349f86b01870b9c416b5474b93d08f
yaswantS542297 <77638984+yaswantS542297@users.noreply.github.com>
2021-03-24 19:37:42 -0500

    updated app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mydatarouter = require('./require/mydata');
+var mydatarouter = require('./routes/mydata');
 
 var app = express();
 
